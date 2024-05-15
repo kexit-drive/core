26b115fc4974994832449fdb5b1fe719be1c995e
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2024-05-15 13:32:42 +0300

    fixed redirect after login

--- src/main/java/kpi/zaranik/kexitdrive/core/config/security/SecurityConfig.java
+++ src/main/java/kpi/zaranik/kexitdrive/core/config/security/SecurityConfig.java
@@ -2,13 +2,17 @@ package kpi.zaranik.kexitdrive.core.config.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +23,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .oauth2Login(withDefaults())
+            .oauth2Login(login -> login.successHandler(customOAuth2LoginSuccessHandler()))
             .oauth2Client(withDefaults())
             .authorizeHttpRequests(requests ->
                 requests
@@ -27,6 +31,13 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    AuthenticationSuccessHandler customOAuth2LoginSuccessHandler() {
+        return (HttpServletRequest request, HttpServletResponse response, Authentication authentication) -> {
+            response.sendRedirect("/");
+        };
     }
 
 }
